6065663924148a68cd1280467c994be8998cb098
kuco23 <nseverkar@gmail.com>
2019-09-24 18:42:00 +0200

    distribution files

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'pokerlib',
+  packages = ['pokerlib'],
+  version = '0.1',
+  license='gpl-3.0',
+  description = 'python poker library',
+  author = 'Nejc Ševerkar',
+  author_email = 'nseverkar@gmail.com',
+  url = 'https://github.com/kuco23/pokerlib',
+  download_url = 'https://github.com/kuco23/pokerlib/archive/v_01-alpha.tar.gz',    # I explain this later on
+  keywords = ['python', 'poker', 'library'],
+  install_requires = [],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: Free For Home Use',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7'
+  ],
+)
