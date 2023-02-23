795ba5af1130d1d54bef29367fba47b29c2231ca
Nejc Ševerkar <nseverkar@gmail.com>
2023-02-23 16:15:06 +0100

    pokerface-pokerlib time comparison

--- tests/handparser_against_pokerface.py
+++ tests/handparser_against_pokerface.py
@@ -3,10 +3,11 @@ sys.path.append('../pokerlib')
 
 from random import sample
 from pokerlib import HandParser
-from pokerlib.enums import Rank, Suit, Hand
+from pokerlib.enums import Rank, Suit
 from pokerface import StandardEvaluator, parse_cards
 
 n_tests = 10000
+n_timeit_tests = 10000
 
 POKERFACE_RANK = ['2','3','4','5','6','7','8','9','T','J','Q','K','A']
 POKERFACE_SUIT = ['s', 'c', 'd', 'h']
@@ -35,3 +36,31 @@ for i in range(n_tests):
     assert (pokerlib_hand_1 > pokerlib_hand_2) is (pokerface_hand_1 > pokerface_hand_2)
     assert (pokerlib_hand_1 < pokerlib_hand_2) is (pokerface_hand_1 < pokerface_hand_2)
     assert (pokerlib_hand_1 == pokerlib_hand_2) is (pokerface_hand_1 == pokerface_hand_2)
+
+def pokerfaceComparisonTest():
+    seed_1 = sample(range(len(POKERLIB_CARDS)), 7)
+    seed_2 = sample(range(len(POKERLIB_CARDS)), 7)
+    pokerface_hand_1 = indexedPokerfaceHand(seed_1)
+    pokerface_hand_2 = indexedPokerfaceHand(seed_2)
+    return (
+        pokerface_hand_1 > pokerface_hand_2,
+        pokerface_hand_1 < pokerface_hand_2,
+        pokerface_hand_1 == pokerface_hand_2)
+
+def pokerlibComparisonTest():
+    seed_1 = sample(range(len(POKERLIB_CARDS)), 7)
+    seed_2 = sample(range(len(POKERLIB_CARDS)), 7)
+    pokerlib_hand_1 = indexedPokerlibHand(seed_1)
+    pokerlib_hand_2 = indexedPokerlibHand(seed_2)
+    return (
+        pokerlib_hand_1 > pokerlib_hand_2,
+        pokerlib_hand_1 < pokerlib_hand_2,
+        pokerlib_hand_1 == pokerlib_hand_2)
+
+from timeit import timeit
+
+pokerface_time = timeit(pokerfaceComparisonTest, number=n_timeit_tests)
+pokerlib_time = timeit(pokerlibComparisonTest, number=n_timeit_tests)
+
+print(f"pokerface parsing time {pokerface_time} for n={n_timeit_tests}")
+print(f"pokerlib parsing time {pokerlib_time} for n={n_timeit_tests}")
