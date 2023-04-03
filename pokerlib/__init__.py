e0992092483078dd0242f0d7c5a11c576896ab37
Nejc Ševerkar <nseverkar@gmail.com>
2023-04-03 22:59:55 +0200

    v2.0.0

--- pokerlib/__init__.py
+++ pokerlib/__init__.py
@@ -7,7 +7,7 @@ from ._round import Round
 from ._table import Table
 
 __title__ = 'pokerlib'
-__version__ = '1.0.0'
+__version__ = '2.0.0'
 __description = 'Python poker library'
 
 __author__ = 'Nejc Ševerkar'
